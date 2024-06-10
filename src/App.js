c3a031bdd00974d25d886f648acfa869cac0b4c1
arsalan68 <arsalankhan8468@gmail.com>
2024-06-10 10:39:33 -0400

    qwe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +11,7 @@ function App() {
         <div style={{height:"100px"}}>
         <Navbar />
         </div>
-
+     
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
